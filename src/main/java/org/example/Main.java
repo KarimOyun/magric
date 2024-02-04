7086b1ee4097e68c8d69c060d2c4354296b704ad
Karim <oyun.k@list.ru>
2024-02-04 14:41:43 +0300

    Calculator

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        System.out.println("Введите первое число: ");
+        float a = scanner.nextFloat();
+
+        System.out.println("Укажите операцию: ");
+        char operation = scanner.next().charAt(0);
+
+        System.out.println("Введите второе число: ");
+        float b = scanner.nextFloat();
+
+            if (operation == '+') {
+                float result = a + b;
+                System.out.println("Результат: " + a + " + " + b + " = " + result);
+            } else if (operation == '-') {
+                float result = a - b;
+                System.out.println("Результат: " + a + " - " + b + " = " + result);
+            } else if (operation == '*') {
+                float result = a * b;
+                System.out.println("Результат: " + a + " * " + b + " = " + result);
+            } else if (operation == '/') {
+                float result = a / b;
+                System.out.println("Результат: " + a + " / " + b + " = " + result);
+            } else {
+                System.out.println("Указанная операция не поддерживается.");
+            }
+        }
+    }
