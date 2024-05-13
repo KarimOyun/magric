c0e608800cd003477cf3967c4ae961ad03a8fa15
KarimOyun <oyun.k@list.ru>
2024-05-13 13:01:58 +0300

    Delete_dublicate_namesout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите: ");
-        System.out.print("Введите: ");
         float a = scanner.nextFloat();
 
         System.out.println("Укажите операцию сюда: ");
